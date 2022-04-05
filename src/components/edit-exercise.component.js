efb848b1e8abea70f8fae32a68340d0f87c25d2d
Chirag Simkhada <chiragsimkhada@gmail.com>
2022-04-05 14:34:02 +0545

    Added Form Frontend

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class EditExercise extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Edit Exercises</h3>
+      </div>
+    );
+  }
+}
+
+export default EditExercise;
---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ExercisesList extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Exercises List</h3>
+      </div>
+    );
+  }
+}
+
+export default ExercisesList;
