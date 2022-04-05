2cf483683ed22b1a5ccf38077fd96d0eea03e1d7
Chirag Simkhada <chiragsimkhada@gmail.com>
2022-04-05 12:07:12 +0545

    Started working in frontend

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -25,4 +25,32 @@ router.route("/add").post((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
+router.route("/:id").get((req, res) => {
+  Exercise.findById(req.params.id)
+    .then((exer) => res.json(exer))
+    .catch((err) => res.status(400).json("Error " + err));
+});
+
+router.route("/:id").delete((req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Deleted Successfully"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/update/:id").post((req, res) => {
+  Exercise.findById(req.params.id)
+    .then((exercise) => {
+      (exercise.username = req.body.username),
+        (exercise.description = req.body.description),
+        (exercise.duration = Number(req.body.duration)),
+        (exercise.date = Date.parse(req.body.date));
+
+      exercise
+        .save()
+        .then(() => res.json("Updated Successfully"))
+        .catch((err) => res.status(400).json("Error " + err));
+    })
+    .catch((error) => res.status(400).json("Error " + error));
+});
+
 module.exports = router;
